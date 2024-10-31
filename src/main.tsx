eb0a681270f764606af40fa17824338b8688aa59
Jean Fernandes <104158771+jeansfernandesx@users.noreply.github.com>
2024-10-31 15:55:25 -0300

    configs material ui themes, cssbaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material';
 
 import App from './App.tsx'
+import theme from './theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    <CssBaseline />
+    </ThemeProvider>
   </StrictMode>,
 )
